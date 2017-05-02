4f9ffa644b65bd3cdc9be200ab7536d61a945fd9
Степан Мирошин <microshine@MacBook-Pro-Stepan.local>
2017-05-02 18:06:40 +0300

    Fix Safari version bug

--- src/helper.ts
+++ src/helper.ts
@@ -23,7 +23,7 @@ export function BrowserInfo() {
         res.version = /chrome\/([\d\.]+)/i.exec(userAgent) ![1];
     } else if (/safari/i.test(userAgent)) {
         res.name = "Safari";
-        res.version = /([\d\.]+) safari/i.exec(userAgent) ![1];
+        res.version = /version\/([\d\.]+)/i.exec(userAgent) ![1];
     } else if (/firefox/i.test(userAgent)) {
         res.name = "Firefox";
         res.version = /firefox\/([\d\.]+)/i.exec(userAgent) ![1];
